0a25a961ea9e8864a05a529d9fd992608f3a3279
Wabri <gabriele.puliti@gmail.com>
2017-10-21 12:56:21 +0200

    Added test exactly Id and salary called in makePayment

--- PayRoll/src/test/java/wabri/PayRoll/PayRollTest.java
+++ PayRoll/src/test/java/wabri/PayRoll/PayRollTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 public class PayRollTest {
 
@@ -58,7 +59,15 @@ public class PayRollTest {
 		
 		assertNumberOfPayments(2);
 		
-		verify(bankService,times(2)).makePayment(anyString(), anyInt());
+		ArgumentCaptor<String> idCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<Integer> salaryCaptor = ArgumentCaptor.forClass(Integer.class);
+		
+		verify(bankService,times(2)).makePayment(idCaptor.capture(), salaryCaptor.capture());
+		
+		assertEquals("ID1", idCaptor.getAllValues().get(0));
+		assertEquals("ID2", idCaptor.getAllValues().get(1));
+		assertEquals(200, salaryCaptor.getAllValues().get(0).intValue());
+		assertEquals(400, salaryCaptor.getAllValues().get(1).intValue());
 	}
 
 	private void assertNumberOfPayments(int expected) {
